39b524f2be80d00212490ffbe7704a5618e7f0c1
atcupps <91998663+atcupps@users.noreply.github.com>
2023-12-25 22:25:30 -0500

    Create interactive course searching

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,98 +1,31 @@
-<script>
+<script lang="ts">
     import { ptLinkFromSlug } from './professors';
     import { formatClasstime } from './formatting';
-    // import { getCourseLookup, searchCourses } from './courseSearch';
+    import { getCourseLookup, searchCourses } from './courseSearch';
+    import CourseListing from './CourseListing.svelte';
 
+    // Load profs and depts data from `+page.ts`
     export let data;
-    let professors = data.professors;
-    let depts = data.departments;
+    let professors: Professor[] = data.professors;
+    let depts: Department[] = data.departments;
 
-    // const courseLookup = getCourseLookup(depts);
+    // Create course lookup table
+    const courseLookup = getCourseLookup(depts);
+
+    // Variable and function for handling course search input
+    let searchInput = '';
+    let searchResults: Course[] = [];
+    function handleInput() {
+        searchResults = searchCourses(searchInput, courseLookup);
+    }
 </script>
 
 <h1>Jupiterp</h1>
 
 <p>The ultimate course schedule planning tool for UMD students.</p>
 
-<h2>Professors:</h2>
-<ul>
-{#each professors as { name, slug, average_rating }}
-    <li>
-        <a href={ ptLinkFromSlug(slug) } target="_blank">{ name }</a>: { average_rating }
-    </li>
-{/each}
-</ul>
+<input type="text" bind:value={searchInput} on:input={handleInput}>
 
-<h2>Courses:</h2>
-{#each depts as { name, courses }}
-    <h3>{name}</h3>
-    <ul>
-        {#each courses as { code, name, credits, gen_eds, description, sections }}
-            <li>
-                <b>{code}: </b>{name}
-                <ul>
-                    <li>
-                        Credits:
-                        {#if 'Amount' in credits}
-                            {credits.Amount}
-                        {:else if 'Range' in credits}
-                            {credits.Range[0]} - {credits.Range[1]}
-                        {/if}
-                    </li>
-                    <li>
-                        GenEds: 
-                        {#if gen_eds == null}
-                            None
-                        {:else}
-                            <ul>
-                                {#each gen_eds as genEd}
-                                    <li>{genEd}</li>
-                                {/each}
-                            </ul>
-                        {/if}
-                    </li>
-                    <li>{description}</li>
-                    <li>
-                        Sections:
-                        {#if sections == null}
-                            None
-                        {:else}
-                            <ul>
-                                {#each sections as { sec_code, instructors, class_meetings }}
-                                    {sec_code}:
-                                    <ul>
-                                        <li>Instructors: { instructors }</li>
-                                        <ul>
-                                            {#each class_meetings as meeting}
-                                                <li>
-                                                    {#if typeof meeting === 'string'}
-                                                        {meeting}
-                                                    {:else if 'OnlineSync' in meeting}
-                                                        Online Sync:
-                                                        {formatClasstime(meeting.OnlineSync)}
-                                                    {:else}
-                                                        {#if meeting.InPerson.classtime == null}
-                                                            Class time: TBA
-                                                        {:else}
-                                                            {formatClasstime(meeting.InPerson.classtime)}, 
-                                                            {#if meeting.InPerson.location == null}
-                                                                Location: TBA
-                                                            {:else}
-                                                                {meeting.InPerson.location[0]}
-                                                                {meeting.InPerson.location[1]}
-                                                            {/if}
-                                                        {/if}
-                                                    {/if}
-                                                </li>
-                                            {/each}
-                                        </ul>
-                                    </ul>
-                                {/each}
-                            </ul>
-                        {/if}
-                    </li>
-                </ul>
-            </li>
-        {/each}
-    </ul>
+{#each searchResults as courseMatch}
+    <CourseListing course={courseMatch}/>
 {/each}
