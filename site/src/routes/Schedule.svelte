8ae693886f55e5343622f9d75cd2ea218649b0d0
atcupps <91998663+atcupps@users.noreply.github.com>
2023-12-29 22:37:09 -0500

    Complete barebones schedule UI

--- site/src/routes/Schedule.svelte
+++ site/src/routes/Schedule.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { schedulify } from './schedule';
+    import ClassMeeting from './ClassMeeting.svelte';
 
     export let selections: ScheduleSelection[] = [];
 
@@ -11,3 +12,51 @@
     }
 </script>
 
+<div>
+    <table style="border: 1px solid black;">
+        <tr style="border: 1px solid black;">
+            <th style="border: 1px solid black;">Monday</th>
+            <th style="border: 1px solid black;">Tuesday</th>
+            <th style="border: 1px solid black;">Wednesday</th>
+            <th style="border: 1px solid black;">Thursday</th>
+            <th style="border: 1px solid black;">Friday</th>
+            {#if schedule.other.length > 0}
+                <th style="border: 1px solid black;">Other</th>
+            {/if}
+        </tr>
+        <tr style="border: 1px solid black;">
+            <td style="border: 1px solid black;">
+                {#each schedule.monday as classMeeting}
+                    <ClassMeeting meeting={classMeeting} />
+                {/each}
+            </td>
+            <td style="border: 1px solid black;">
+                {#each schedule.tuesday as classMeeting}
+                    <ClassMeeting meeting={classMeeting} />
+                {/each}
+            </td>
+            <td style="border: 1px solid black;">
+                {#each schedule.wednesday as classMeeting}
+                    <ClassMeeting meeting={classMeeting} />
+                {/each}
+            </td>
+            <td style="border: 1px solid black;">
+                {#each schedule.thursday as classMeeting}
+                    <ClassMeeting meeting={classMeeting} />
+                {/each}
+            </td>
+            <td style="border: 1px solid black;">
+                {#each schedule.friday as classMeeting}
+                    <ClassMeeting meeting={classMeeting} />
+                {/each}
+            </td>
+            {#if schedule.other.length > 0}
+                <td style="border: 1px solid black;">
+                    {#each schedule.other as classMeeting}
+                        <ClassMeeting meeting={classMeeting} />
+                    {/each}
+                </td>
+            {/if}
+        </tr>
+    </table>
+</div>
