e615cfb6d157060171f391cc623680efc745735c
atcupps <91998663+atcupps@users.noreply.github.com>
2024-01-05 21:25:06 -0500

    Show 5 star rating for profs with ratings

--- site/src/routes/CourseSearch.svelte
+++ site/src/routes/CourseSearch.svelte
@@ -28,7 +28,8 @@
     const profsLookup = getProfsLookup(professors);
 </script>
 
-<div class='flex flex-col min-w-[300px] h-full border-r-2 border-divBorderLight dark:border-divBorderDark border-solid py-2 pr-2'>
+<div class='flex flex-col min-w-[320px]
+        h-full border-r-2 border-divBorderLight dark:border-divBorderDark border-solid py-2 pr-2'>
     <div class='w-full border-solid border-b-2 border-divBorderLight dark:border-divBorderDark'>
         <input type="text" bind:value={searchInput} on:input={handleInput}
             class="border-solid border-2 border-outlineLight dark:border-outlineDark 
--- site/src/routes/InstructorListing.svelte
+++ site/src/routes/InstructorListing.svelte
@@ -5,7 +5,16 @@
 
     export let instructor: string = 'No instructor';
     export let profs: Record<string, Professor>;
-    console.log(profs);
+
+    // Convert rating to a percentage for CSS
+    function convertRating(rating: number | null): number {
+        if (rating == null) {
+            throw Error(
+                'Rating was null in `convertRating`; this should never happen!'
+            );
+        }
+        return rating * 20;
+    }
 
     function handleLinkClick(event: MouseEvent) {
         // Prevent the event from propagating to the button
@@ -21,7 +30,33 @@
                     on:click={handleLinkClick}>
             {instructor}
         </a>
+        <span style='--rating: {convertRating(profs[instructor].average_rating) + '%'}'
+                class='text-[10px] align-[2px] text-orange font-bold stars'>
+            ★★★★★
+        </span>
     {:else}
         {instructor}
     {/if}
 </div>
+
+<style>
+    .stars {
+        background: rgb(246,116,60);
+        background: linear-gradient(90deg, 
+                            rgba(246,116,60,1) 0%, 
+                            rgba(246,116,60,1) var(--rating), 
+                            rgba(115, 53, 26, 1) var(--rating), 
+                            rgba(115, 53, 26, 1) 100%);
+        
+        /* Set the background size and repeat properties. */
+        background-size: 100%;
+        background-repeat: repeat;
+
+        /* Use the text as a mask for the background. */
+        /* This will show the gradient as a text color rather than element bg. */
+        background-clip: text;
+        -webkit-text-fill-color: transparent; 
+        -moz-background-clip: text;
+        -moz-text-fill-color: transparent;
+    }
+</style>
