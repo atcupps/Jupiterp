544dbe7636a5b4a9b9505b0776a6f45134b7441b
atcupps <at.cupps@gmail.com>
2024-01-13 21:08:43 -0500

    Reduce Course Search width

--- site/src/routes/CourseListing.svelte
+++ site/src/routes/CourseListing.svelte
@@ -24,7 +24,7 @@
     </div>
 
     <!-- Course title -->
-    <div class='max-w-[314px] 2xl:max-w-[434px] text-sm 2xl:text-base truncate'>
+    <div class='max-w-[314px] 2xl:max-w-[394px] text-sm 2xl:text-base truncate'>
         {course.name}
     </div>
 
