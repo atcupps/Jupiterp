26e5208af02e14b4472ecfe5faae4c46eb0856ee
atcupps <at.cupps@gmail.com>
2024-01-13 20:54:12 -0500

    Increase time size

--- site/src/routes/TimeLine.svelte
+++ site/src/routes/TimeLine.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <div class='w-full absolute h-[28px]' style='top: {position * 100}%'>
-    <div class='text-xs font-light absolute text-right w-[34px]' 
+    <div class='text-xs 2xl:text-sm font-light absolute text-right w-[34px]' 
             style='left: -8px; top: 50%; transform: translateY(-50%);'>
         {number}
     </div>
     <div class='h-[1px] bg-divBorderLight 
-        dark:bg-divBorderDark absolute' 
-        style='top: 50%; left: 32px; width: calc(100% - 32px);'/>
+        dark:bg-divBorderDark absolute left-8 2xl:left-9' 
+        style='top: 50%; width: calc(100% - 32px);'/>
 </div>
