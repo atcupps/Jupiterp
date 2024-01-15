56f5e0c90f126cf5bf6555cd36e5eea48d845ba2
atcupps <91998663+atcupps@users.noreply.github.com>
2024-01-14 21:29:38 -0500

    Fix issue

--- site/src/routes/ScheduleDay.svelte
+++ site/src/routes/ScheduleDay.svelte
@@ -15,7 +15,7 @@
     </div>
     <div class='relative' 
         style='height: {bgHeight}px; top: 14px'>
-        {#each classes as classMeeting}
+        {#each classes as classMeeting (classMeeting.instructors)}
             <ClassMeeting meeting={classMeeting}
                 bind:earliestClassStart bind:latestClassEnd />
         {/each}
