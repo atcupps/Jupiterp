ad8fdf400b57b416385690beecd46aa1ec05e159
atcupps <91998663+atcupps@users.noreply.github.com>
2024-01-14 19:07:30 -0500

    Add beta to logo, add title to page

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -5,6 +5,9 @@
 <div class='flex flex-col h-full w-full text-textLight dark:text-textDark'>
     <div class='flex h-16 border-b-2 border-divBorderLight dark:border-divBorderDark'>
         <img src='logo.svg' alt='Jupiterp logo'/>
+        <span class='absolute left-[17.2rem] top-1 text-xl italic text-orange'>
+            beta
+        </span>
     </div>
     <slot/>
 </div>
