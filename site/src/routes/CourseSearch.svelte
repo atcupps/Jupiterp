32124153351f003e616857474dae2d1d4fd0a758
atcupps <91998663+atcupps@users.noreply.github.com>
2024-01-22 20:13:57 -0500

    Make buttons bigg:

--- site/src/routes/CourseSearch.svelte
+++ site/src/routes/CourseSearch.svelte
@@ -39,16 +39,15 @@
 </script>
 
 <!-- Button to toggle course search on mobile -->
-<button class='fixed h-4 w-4 top-7 left-4 visible lg:hidden z-[51]'
-        style='transform: translateY(-50%);'
+<button class='fixed h-5 w-5 top-3 left-5 visible lg:hidden z-[51]'
         on:click={() => {courseSearchSelected = !courseSearchSelected}}>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
-            class='visible h-4 w-4 transition 
+            class='visible h-full w-full transition 
                     fill-textLight dark:fill-textDark'
             class:hidden={courseSearchSelected}>
                 <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
-            class='visible h-4 w-4 transition 
+            class='visible h-full w-full transition 
                     fill-textLight dark:fill-textDark'
             class:hidden={!courseSearchSelected}>
                 <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
--- site/src/routes/SiteLinks.svelte
+++ site/src/routes/SiteLinks.svelte
@@ -15,16 +15,15 @@
 </div>
 
 <!-- Button to toggle course search on mobile -->
-<button class='absolute h-4 w-4 top-7 right-4 visible lg:hidden z-[52]'
-        style='transform: translateY(-50%);'
+<button class='absolute h-6 w-6 top-3 right-5 visible lg:hidden z-[52]'
         on:click={() => {siteLinksSelected = !siteLinksSelected}}>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
-            class='visible h-4 w-4 transition 
+            class='visible h-full w-full transition 
                     fill-textLight dark:fill-textDark'
             class:hidden={siteLinksSelected}>
                 <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
-            class='visible h-4 w-4 transition 
+            class='visible h-full w-full transition 
                     fill-textLight dark:fill-textDark'
             class:hidden={!siteLinksSelected}>
                 <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
