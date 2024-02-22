bc8f0bc846f998d84cc2d5e9b8d8a0309c044dd3
atcupps <91998663+atcupps@users.noreply.github.com>
2024-02-22 13:38:43 -0500

    Correct color for description

--- site/src/routes/Schedule.svelte
+++ site/src/routes/Schedule.svelte
@@ -10,7 +10,6 @@ Copyright (C) 2024 Andrew Cupps
     } from './schedule';
     import ScheduleDay from './ScheduleDay.svelte';
     import ScheduleBackground from './ScheduleBackground.svelte';
-    import { getCourseLookup } from './courseSearch';
     import { formatCredits, testudoLink } from './formatting';
 
     export let hoveredSection: ScheduleSelection | null;
@@ -149,9 +148,12 @@ Copyright (C) 2024 Andrew Cupps
                 <button class='absolute h-7 w-7 top-0 right-0
                         2xl:top-1 2xl:right-1 justify-center'
                         on:click={() => {showCourseInfo = null}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
-                        class='absolute h-5 w-5 2xl:h-6 2xl:w-6 top-[50%] left-[50%]'
-                        style='transform: translateX(-50%) translateY(-50%);'>
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                         viewBox="0 0 384 512"
+                         class='absolute h-5 w-5 2xl:h-6 2xl:w-6 
+                                top-[50%] left-[50%] stroke-black fill-black 
+                                dark:stroke-white dark:fill-white'
+                         style='transform: translateX(-50%) translateY(-50%);'>
                         <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
                 </button>
 
