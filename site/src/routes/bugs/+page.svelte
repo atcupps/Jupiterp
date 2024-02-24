d373b7689bed47029bb22002fccc9c3bf000651e
atcupps <91998663+atcupps@users.noreply.github.com>
2024-02-24 13:57:49 -0500

    Add bug report page

---
+++ site/src/routes/bugs/+page.svelte
@@ -0,0 +1,16 @@
+<!-- 
+This file is part of Jupiterp. For terms of use, please see the file
+called LICENSE at the top level of the Jupiterp source tree (online at
+https://github.com/atcupps/Jupiterp/LICENSE).
+Copyright (C) 2024 Andrew Cupps
+ -->
+<div class='text-textLight dark:text-white py-2 px-8 overflow-y-scroll
+            flex justify-center w-full
+            top-[3rem] lg:top-[3.5rem] xl:top-[4rem] bottom-0 fixed'>
+    <iframe 
+        class="w-full h-full"
+        title="Jupiterp Issue/Bug Report Form" 
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfCNBmYwIRS2eaqw9PPRJQLahm2H-UnMs8LkMYDHyfCdPnr2w/viewform?embedded=true">
+            Loading…
+    </iframe>
+</div>
