3c4c25e88804fe600e455947d02431f98efd677f
atcupps <91998663+atcupps@users.noreply.github.com>
2024-04-09 13:09:31 -0400

    Basic dropdown demo

--- site/src/components/course-planner/schedule/ScheduleManager.svelte
+++ site/src/components/course-planner/schedule/ScheduleManager.svelte
@@ -9,39 +9,51 @@ Copyright (C) 2024 Andrew Cupps
         AngleRightOutline, AngleDownOutline, PlusOutline
     } from "flowbite-svelte-icons";
     import ScheduleOptionsDropdown from "./ScheduleOptionsDropdown.svelte";
+    import { slide } from 'svelte/transition';
 
     let dropdownOpen: boolean = false;
 
     let scheduleNameInput: string = '';
-    let scheduleName: string = 'My Schedule'
+    let scheduleName: string = 'Schedule 1'
     $: if (scheduleNameInput.length > 0) {
         scheduleName = scheduleNameInput;
         console.log(scheduleName);
     } else {
-        scheduleName = 'My Schedule';
+        scheduleName = 'Schedule 1';
         console.log(scheduleName);
     }
 
     export let selections: ScheduleSelection[];
 </script>
 
-<div class='text-sm pb-1 font-light w-full static flex 
-                                        flex-row items-center justify-start'>
-    <div class="hover:bg-hoverLight dark:hover:bg-hoverDark rounded-md 
-                    grow flex flex-row items-center justify-start h-7">
-        <button on:click={() => {dropdownOpen = !dropdownOpen}}>
-            {#if !dropdownOpen}
-                <AngleRightOutline class="w-5 h-5" />
-            {:else}
-                <AngleDownOutline class="w-5 h-5" />
-            {/if}
+<div class='flex w-full flex-col mb-1'>
+    <div class='text-sm font-light w-full static flex 
+                flex-row items-center justify-start'>
+        <div class="hover:bg-hoverLight dark:hover:bg-hoverDark rounded-md 
+                        grow flex flex-row items-center justify-start h-7">
+            <button on:click={() => {dropdownOpen = !dropdownOpen}}>
+                {#if !dropdownOpen}
+                    <AngleRightOutline class="w-5 h-5" />
+                {:else}
+                    <AngleDownOutline class="w-5 h-5" />
+                {/if}
+            </button>
+            <input class="text-inherit grow text-left bg-bgLight dark:bg-bgDark
+                        px-0.5 rounded-sm cursor-text" placeholder="Schedule 1"
+                        bind:value={scheduleNameInput}>
+            <ScheduleOptionsDropdown />
+        </div>
+        <button class="rounded-md hover:bg-hoverLight dark:hover:bg-hoverDark h-7">
+            <PlusOutline class="w-5 h-5 px-0.5" />
         </button>
-        <input class="text-inherit grow text-left bg-bgLight dark:bg-bgDark
-                    px-0.5 rounded-sm cursor-text" placeholder="My Schedule"
-                    bind:value={scheduleNameInput}>
-        <ScheduleOptionsDropdown />
     </div>
-    <button class="rounded-md hover:bg-hoverLight dark:hover:bg-hoverDark h-7">
-        <PlusOutline class="w-5 h-5 px-0.5" />
-    </button>
+
+    {#if dropdownOpen}
+        <button class='text-sm mr-5 ml-4 pl-1 flex hover:bg-hoverLight
+                        dark:hover:bg-hoverDark text-left flex-row rounded-md
+                        h-6 items-center' transition:slide>
+            <div class='grow'>Alan's schedule</div>
+            <ScheduleOptionsDropdown />
+        </button>
+    {/if}
 </div>
