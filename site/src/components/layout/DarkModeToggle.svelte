fe7ce59f395f82665d0f7071475d0d3538e59058
atcupps <91998663+atcupps@users.noreply.github.com>
2024-05-09 17:44:42 -0400

    Rudimentary dark mode toggle

---
+++ site/src/components/layout/DarkModeToggle.svelte
@@ -0,0 +1,15 @@
+<script lang='ts'>
+    function toggleDarkMode() {
+        const isDark: boolean = (localStorage.theme === 'dark');
+        if (isDark) {
+            localStorage.setItem('theme', 'light');
+        } else {
+            localStorage.setItem('theme', 'dark');
+        }
+
+        document.documentElement.classList.toggle('dark');
+    }
+</script>
+
+<button class='w-5 h-5 bg-orange' 
+        on:click={toggleDarkMode} />
