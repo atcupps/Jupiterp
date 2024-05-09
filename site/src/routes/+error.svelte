ac1fb6da605fd1682aec0333709d8f7998367be8
atcupps <91998663+atcupps@users.noreply.github.com>
2024-05-08 20:52:53 -0400

    Create an error page

---
+++ site/src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script>
+// @ts-nocheck
+
+	import { page } from '$app/stores';
+</script>
+
+<div class='error'>
+    <h1 class='text-black dark:text-white error-message'>
+        Error {$page.status}:
+        {$page.error.message}
+    </h1>
+</div>
+
+<style>
+    .error {
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        transform: translateX(-50%) translateY(-50%);
+        width: 100%;
+        padding-left: 4rem;
+    }
+
+    .error-message {
+        font-size: 3rem;
+    }
+</style>
