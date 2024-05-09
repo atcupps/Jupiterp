cb4cc910cf3f19cb123748ae273106fe191420cf
atcupps <91998663+atcupps@users.noreply.github.com>
2024-05-09 19:15:00 -0400

    Create sliding toggle switch for dark mode

--- site/src/components/layout/DarkModeToggle.svelte
+++ site/src/components/layout/DarkModeToggle.svelte
@@ -1,4 +1,7 @@
 <script lang='ts'>
+    import { SunOutline } from "flowbite-svelte-icons";
+    import { MoonOutline } from "flowbite-svelte-icons";
+
     function toggleDarkMode() {
         const isDark: boolean = (localStorage.theme === 'dark');
         if (isDark) {
@@ -11,5 +14,21 @@
     }
 </script>
 
-<button class='w-5 h-5 bg-orange' 
-        on:click={toggleDarkMode} />
+<button class='w-9 h-5 mx-4 bg-transparent border-solid border rounded-[10px]
+                border-outlineLight dark:border-outlineDark xl:translate-y-[10%]
+                hover:border-black dark:hover:border-white
+                hover:transition-colors'
+        on:click={toggleDarkMode} >
+    <div class='relative rounded-[8px] h-4 w-4 top-[50%] translate-y-[-55%]
+                left-0 dark:translate-x-[1.12rem]
+                bg-bgSecondaryLight dark:bg-hoverDark transform
+                transition-transform duration-300 ease-in-out'>
+        <SunOutline class='relative left-[50%] top-[50%]
+                            w-3 h-3
+                            translate-x-[-55%] translate-y-[-50%]
+                            text-black visible dark:hidden' />
+        <MoonOutline class='relative w-3 h-3 left-[50%] top-[50%]
+                            translate-x-[-50%] translate-y-[-50%]
+                            text-white hidden dark:block' />
+    </div>
+</button>
