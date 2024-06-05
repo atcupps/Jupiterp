1232c5bd37186aad3c6d5e240291fc6c905433b2
atcupps <91998663+atcupps@users.noreply.github.com>
2024-06-04 21:08:02 -0400

    Create snazzy dropdown in navbar

--- site/src/components/layout/ExpandableNavBarElement.svelte
+++ site/src/components/layout/ExpandableNavBarElement.svelte
@@ -5,18 +5,27 @@ https://github.com/atcupps/Jupiterp/LICENSE).
 Copyright (C) 2024 Andrew Cupps
 -->
 <script lang='ts'>
+    import { AngleDownOutline } from "flowbite-svelte-icons";
+
     export let link: string;
     export let text: string;
     export let target: string = '_self';
     export let fullWidth: boolean = false;
     export let isOnPage: boolean = false;
-    export let reduceXMargin: boolean = false;
+
+    let showExpandedLinks: boolean = false;
 </script>
 
-<div class='mx-4 px-1 font-normal'
-        class:mx-0={reduceXMargin}>
+<!-- Ignoring a11y because `focus` supplements `mouse` -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div class='mx-4 px-1 font-normal relative'
+        on:mouseenter={() => {showExpandedLinks = true;}}
+        on:mouseleave={() => {showExpandedLinks = false;}}
+        on:focusin={() => {showExpandedLinks = true}}
+        on:focusout={() => {showExpandedLinks = false}}>
     <a href={link} target={target}
-        class='transition text-nowrap'
+        class='transition inline-flex items-center'>
+        <span
             class:w-[90%]={fullWidth}
             class:siteLinkUnderline={isOnPage}
             class:text-orange={isOnPage}
@@ -24,8 +33,17 @@ Copyright (C) 2024 Andrew Cupps
             class:dark:hover:text-lightOrange={!isOnPage}
             class:text-textLight={!isOnPage}
             class:dark:text-white={!isOnPage}>
-        {text}
+                {text}
+        </span>
+        <AngleDownOutline class='w-3 h-3 ml-1 
+                                text-textLight dark:text-textDark' />
     </a>
+    <div class='absolute top-full min-w-12 flex flex-col bg-bgLight
+                dark:bg-bgDark p-2 translate-x-[-12%] rounded-lg visible
+                border-2 border-divBorderLight dark:border-divBorderDark'
+            class:hidden={!showExpandedLinks}>
+        <slot/>
+    </div>
 </div>
 
 <style>
