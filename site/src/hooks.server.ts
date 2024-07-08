a5e97177543454e125c47a9540bfdfd2d3edb9ec
atcupps <91998663+atcupps@users.noreply.github.com>
2024-07-08 17:59:55 -0400

    Retry using environment variables with quotations added

--- site/src/hooks.server.ts
+++ site/src/hooks.server.ts
@@ -3,7 +3,7 @@ import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
-  dsn: 'https://a4494d5cf591eeddd496ed18488c5faa@o4507562502782976.ingest.us.sentry.io/4507562512023552',
+  dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1.0, 
 });
 
