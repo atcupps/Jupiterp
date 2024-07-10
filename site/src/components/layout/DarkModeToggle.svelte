d2eb1b1b5edf9e9d01484c276d476daac051008c
atcupps <91998663+atcupps@users.noreply.github.com>
2024-07-10 19:24:00 -0400

    Use static and absolute positionings for dark mode toggle

--- site/src/components/layout/DarkModeToggle.svelte
+++ site/src/components/layout/DarkModeToggle.svelte
@@ -15,11 +15,11 @@
 </script>
 
 <button class='w-9 h-5 mx-4 bg-transparent border-solid border rounded-[10px]
-                border-outlineLight dark:border-outlineDark xl:translate-y-[10%]
-                hover:border-black dark:hover:border-white
-                hover:transition-colors'
+                border-outlineLight dark:border-outlineDark static
+                hover:border-black dark:hover:border-white 
+                hover:transition-colors translate-y-[0%] xl:translate-y-[10%]'
         on:click={toggleDarkMode} >
-    <div class='relative rounded-[8px] h-4 w-4 top-[50%] translate-y-[-55%]
+    <div class='absolute rounded-[8px] h-4 w-4 top-[50%] translate-y-[-55%]
                 left-0 dark:translate-x-[1.12rem]
                 bg-bgSecondaryLight dark:bg-hoverDark transform
                 transition-transform duration-300 ease-in-out'>
