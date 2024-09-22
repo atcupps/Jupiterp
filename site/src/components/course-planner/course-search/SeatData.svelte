5ecc0a82407b167f3b5b3e77ef8aefc455038caf
atcupps <91998663+atcupps@users.noreply.github.com>
2024-09-22 11:34:11 -0400

    Add available seats in section to course search

---
+++ site/src/components/course-planner/course-search/SeatData.svelte
@@ -0,0 +1,17 @@
+<script lang='ts'>
+    import { SeatDataStore } from "../../../stores/CoursePlannerStores";
+
+    export let course: string;
+    export let section: string;
+
+    let seats: number[] | null;
+    SeatDataStore.subscribe((value) => {
+        seats = value[course + '-' + section];
+    })
+</script>
+
+{#if seats}
+    <div class='flex flex-row text-xs 2xl:text-base font-medium w-full'>
+        {seats[0]} / {seats[1]} seats available
+    </div>
+{/if}
