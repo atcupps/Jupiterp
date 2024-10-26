28566f483f3205db27a5e7d19604a99f16694717
caramurphyy <murphylcara@gmail.com>
2024-10-26 13:09:41 -0400

    enable searching for courses by course number

--- site/src/lib/course-planner/CourseSearch.ts
+++ site/src/lib/course-planner/CourseSearch.ts
@@ -54,6 +54,7 @@ export function searchCourses(input: string, courseLookup:
     if (simpleInput.length >= 4) {
         const dept: string = simpleInput.substring(0, 4);
         const deptCourses: Record<string, Course> = courseLookup[dept];
+      
         if (deptCourses != undefined) {
             for (const courseCode in deptCourses) {
                 let shouldBeInResult = true;
@@ -70,6 +71,40 @@ export function searchCourses(input: string, courseLookup:
                 if (shouldBeInResult) {
                     result.push(deptCourses[courseCode]);
                 }
+            }
+        }
+    } 
+    
+    // If the search input is just numbers, match courses with that number
+    const deptList = ["AASP", "AAST", "ABRM", "AGNR", "AGST", "AMSC", "AMST", "ANSC", "ANTH", "AOSC", "ARAB", "ARCH", "AREC", "ARHU", "ARMY", "ARSC", "ARTH", "ARTT", "ASTR", "BCHM", "BIOE", "BIOI", "BIOL", "BIOM", "BIPH", "BISI", "BMGT", "BMSO", "BSCI", "BSOS", "BSST", "BUAC", "BUDT", "BUFN", "BULM", "BUMK", "BUSI", "BUSM", "BUSO", "CBMG", "CCJS", "CHBE", "CHEM", "CHIN", "CHPH", "CHSE", "CINE", "CLAS", "CLFS", "CMLT", "CMSC", "COMM", "CPBE", "CPCV", "CPET", "CPGH", "CPJT", "CPMS", "CPPL", "CPSA", "CPSF", "CPSG", "CPSN", "CPSP", "CPSS", "DANC", "DATA", "ECON", "EDCP", "EDHD", "EDHI", "EDMS", "EDSP", "EDUC", "EMBA", "ENAE", "ENBC", "ENCE", "ENCO", "ENEB", "ENEE", "ENES", "ENFP", "ENGL", "ENMA", "ENME", "ENPM", "ENRE", "ENSE", "ENSP", "ENST", "ENTM", "ENTS", "EPIB", "FGSM", "FIRE", "FMSC", "FREN", "GBHL", "GEMS", "GEOG", "GEOL", "GERS", "GREK", "GVPT", "HACS", "HBUS", "HDCC", "HEBR", "HESI", "HESP", "HGLO", "HHUM", "HISP", "HIST", "HLSA", "HLSC", "HLTH", "HNUH", "HONR", "IDEA", "IMDM", "IMMR", "INAG", "INFM", "INST", "ISRL", "ITAL", "JAPN", "JOUR", "JWST", "KNES", "KORA", "LACS", "LARC", "LATN", "LBSC", "LEAD", "LGBT", "LING", "MAIT", "MATH", "MEES", "MIEH", "MITH", "MLAW", "MSML", "MSQC", "MUED", "MUSC", "NACS", "NAVY", "NEUR", "NFSC", "NIAP", "NIAV", "PEER", "PERS", "PHIL", "PHPE", "PHSC", "PHYS", "PLCY", "PLSC", "PORT", "PSYC", "RDEV", "RELS", "RUSS", "SLAA", "SLLC", "SMLP", "SOCY", "SPAN", "SPHL", "STAT", "SURV", "TDPS", "THET", "TLPL", "TLTC", "UMEI", "UNIV", "URSP", "USLT", "VMSC", "WEID", "WGSS", "XPER"];
+    if (simpleInput.length >= 1 && /^[0-9]+$/i.test(simpleInput)) {
+
+        // get all the courses from every department 
+        const allDeptCourses: Record<string, Course> = {};
+        for (const dept of deptList) {
+            const deptCourses = courseLookup[dept];
+            if (deptCourses !== undefined) {
+                for (const courseCode in deptCourses) {
+                    const uniqueCourseCode = `${dept}-${courseCode}`;
+                    allDeptCourses[uniqueCourseCode] = deptCourses[courseCode];
+                }
+            }
+        }
+
+        for (const courseCode in allDeptCourses) {
+            let shouldBeInResult = true;
+            if (simpleInput.length > courseCode.length) {
+                shouldBeInResult = false;
+            } else {
+                const courseNumber = courseCode.substring(5);
+                for (let i = 0; i < simpleInput.length; i++) { 
+                    if (simpleInput[i] != courseNumber[i]) {
+                        shouldBeInResult = false;
+                    }
+                }
+            }
+            if (shouldBeInResult) {
+                result.push(allDeptCourses[courseCode]);
             }
         }
     }
