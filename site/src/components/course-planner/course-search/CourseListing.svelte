d7b445271a6d56f95d9a5c10b905b055a49c911a
atcupps <91998663+atcupps@users.noreply.github.com>
2025-04-04 17:23:59 -0400

    Add show more button for courses in course search

--- site/src/components/course-planner/course-search/CourseListing.svelte
+++ site/src/components/course-planner/course-search/CourseListing.svelte
@@ -6,7 +6,8 @@ Copyright (C) 2024 Andrew Cupps
 -->
 <script lang="ts">
     import SectionListing from "./SectionListing.svelte";
-    import { formatCredits, getMinCredits } from "../../../lib/course-planner/Formatting";
+    import { formatCredits, getMinCredits, testudoLink } from "../../../lib/course-planner/Formatting";
+    import { slide } from "svelte/transition";
 
     export let course: Course;
 
@@ -17,6 +18,8 @@ Copyright (C) 2024 Andrew Cupps
             class_meetings: ['No Class Meetings']
         }
     }
+
+    let showMoreInfo: boolean = false;
 </script>
 
 <div class='px-2 my-2 bg-bgSecondaryLight dark:bg-bgSecondaryDark 
@@ -38,6 +41,25 @@ Copyright (C) 2024 Andrew Cupps
                     text-sm 2xl:text-base wrap'>
         {course.name}
     </div>
+
+    <button class='text-xs 2xl:text-sm text-left hover:text-outlineDark
+                text-secCodesLight dark:text-secCodesDark'
+            on:click={() => {showMoreInfo = !showMoreInfo}}>
+        {showMoreInfo ? "Show less" : "Show more"}
+    </button>
+
+    {#if showMoreInfo}
+        <div class='text-xs 2xl:text-sm py-1'
+                transition:slide>
+            <a href={testudoLink(course.code)} 
+                class='text-orange underline'
+                target='_blank'>
+                View on Testudo
+            </a>
+            <br>
+            {course.description}
+        </div>
+    {/if}
 
     <!-- Sections -->
     {#if course.sections != null}
