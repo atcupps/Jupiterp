aae7c190863970f7354b8fc8125fae1d45174697
atcupps <91998663+atcupps@users.noreply.github.com>
2025-09-29 13:17:13 -0400

    add loading animation

--- site/src/components/course-planner/course-search/CourseSearch.svelte
+++ site/src/components/course-planner/course-search/CourseSearch.svelte
@@ -180,16 +180,20 @@ Copyright (C) 2024 Andrew Cupps
                 {/each}
             </div>
         {/if}
-
-        {#if isPendingResults}
-            Loading...
-        {/if}
     </div>
     <div class='grow courses-list overflow-y-scroll overflow-x-none
                 px-1 lg:pr-1 lg:pl-0'>
         {#each searchResults as courseMatch (courseMatch.courseCode)}
             <CourseListing course={courseMatch} />
         {/each}
+
+        {#if isPendingResults}
+            <div class='flex justify-center py-4' aria-live='polite'>
+                <span class='h-8 w-8 animate-spin text-center'>
+                    Loading...
+                </span>
+            </div>
+        {/if}
     </div>
 </div>
 
