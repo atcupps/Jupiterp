d38927fef16797a7b5c3367ffb894ac098e7981d
atcupps <91998663+atcupps@users.noreply.github.com>
2025-10-13 22:32:56 -0400

    Tooltip no longer flashes when class difference shows up

--- site/src/components/course-planner/schedule/Tooltip.svelte
+++ site/src/components/course-planner/schedule/Tooltip.svelte
@@ -16,14 +16,15 @@ Copyright (C) 2024 Andrew Cupps
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-mouse-events-have-key-events -->
 <span
-    on:mouseover={() => tooltipVisible = true}
-    on:mouseout={() => tooltipVisible = false}>
+    class="tooltip-trigger underline decoration-dotted cursor-help"
+    on:mouseenter={() => tooltipVisible = true}
+    on:mouseleave={() => tooltipVisible = false}>
     {text}
 </span>
 {#if tooltipVisible}
     <div class="absolute bg-black text-white p-2 text-center rounded-md
-                left-[50%] translate-x-[-50%] text-wrap w-[128px] z-30 top-0">
+                left-[50%] translate-x-[-50%] text-wrap w-[128px] z-30 top-0 
+                pointer-events-none">
         {tooltipText}
     </div>
 {/if}
-  
