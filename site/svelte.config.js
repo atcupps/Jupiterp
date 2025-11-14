c27e23420e9abff13d3bd23baf58775e8ab70285
atcupps <91998663+atcupps@users.noreply.github.com>
2025-11-13 22:02:54 -0500

    try this multi-environment approach

--- site/svelte.config.js
+++ site/svelte.config.js
@@ -1,9 +1,10 @@
-import adapter from '@sveltejs/adapter-node';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: process.env.VERCEL === '1' ? adapterVercel() : adapterNode()
   },
   preprocess: vitePreprocess()
 };
