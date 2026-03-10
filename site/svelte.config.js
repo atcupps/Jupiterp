9a70c57d98bd52285fe78639984e61d2890e84be
UndefinedConcept <94262481+UndefinedConcept@users.noreply.github.com>
2026-03-10 18:16:39 -0400

    Added prettier to eslint

--- site/svelte.config.js
+++ site/svelte.config.js
@@ -3,9 +3,9 @@ import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: process.env.VERCEL === '1' ? adapterVercel() : adapterNode()
-  },
-  preprocess: vitePreprocess()
+	kit: {
+		adapter: process.env.VERCEL === '1' ? adapterVercel() : adapterNode()
+	},
+	preprocess: vitePreprocess()
 };
 export default config;
--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,12 +1,15 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "jupiterp",
-            project: "jupiterp"
-        }
-    }), sveltekit()]
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'jupiterp',
+				project: 'jupiterp'
+			}
+		}),
+		sveltekit()
+	]
 });
